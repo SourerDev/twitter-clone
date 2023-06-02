8550014485a5fe06d15945cddbc1eec8fc95c882
jhoniernem <jhonny30ca@gmail.com>
2023-06-02 16:12:18 -0500

    finished

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -10,6 +10,18 @@ import {
 } from "~/server/api/trpc";
 
 export const tweetRouter = createTRPCRouter({
+  infiniteProfileFeed: publicProcedure.input(z.object({
+    userId: z.string(),
+    limit: z.number().optional(),
+    cursor: z.object({ id: z.string(), createdAt: z.date() }).optional()
+  })).query(async ({ input: { limit = 10, userId, cursor }, ctx }) => {
+
+    const currentUserId = ctx.session?.user.id
+
+    return await getInfiniteTweets({
+      limit, ctx, cursor, whereClause: { userId }
+    })
+  }),
   infiniteFeed: publicProcedure.input(z.object({
     onlyFollowing: z.boolean().optional(),
     limit: z.number().optional(),
@@ -30,6 +42,8 @@ export const tweetRouter = createTRPCRouter({
     .input(z.object({ content: z.string() }))
     .mutation(async ({ input: { content }, ctx }) => {
       const tweet = await ctx.prisma.tweet.create({ data: { content, userId: ctx.session.user.id } })
+
+      void ctx.revalidateSSG?.(`/profiles/${ctx.session.user.id}`)
       return tweet
     }),
   toggleLike: protectedProcedure.input(z.object({
